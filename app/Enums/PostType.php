9737746749a1d1ea56c80a689cb0c6de1221816b
Ian Hannaford <ian@truecommerce.co.uk>
2020-08-19 10:15:55 +0100

    Image Height and Title

--- app/Enums/PostType.php
+++ app/Enums/PostType.php
@@ -8,10 +8,21 @@ use Illuminate\Auth\Access\Gate;
 
 class PostType
 {
-    public function __construct( $type, $title )
+    public $type;
+    public $title;
+    public $prefix_width;
+    public $prefix_height;
+    public $image_width;
+    public $image_height;
+
+    public function __construct( $type, $title, $prefix_width = 100, $prefix_height = 100, $image_width = 250, $image_height = 250 )
     {
         $this->type = $type;
         $this->title = $title;
+        $this->prefix_width = $prefix_width;
+        $this->prefix_height = $prefix_height;
+        $this->image_width = $image_width;
+        $this->image_height = $image_height;
     }
 
     public static function HOME() {
